e8e169ab79a83fe81696cc62e29c94d327caec9f
AndrieievaLena <elenakovalevamod@gmail.com>
2021-12-07 17:50:53 +0200

    Added gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import '../css/01-gallery.css'
+import '../css/common.css'
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryOfImages = document.querySelector('.gallery');
+const cardsImages = onCreateGalleryItem(galleryItems);
+
+galleryOfImages.insertAdjacentElement('beforeend', cardsImages);
+
+function onCreateGalleryItem(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class= "gallery__item" href = '${original}'>
+        <img class = "gallery__image" scr = '${preview}' alt = '${description}'>
+        </a>
+        `
+
+    }).join('')
+}
+new SimpleLightbox('.gallery a', {
+   disableRightClick: true,
+    scrollZoom: false,
+    captionDelay: 250,
+    captionsData: 'alt', 
+});
+
