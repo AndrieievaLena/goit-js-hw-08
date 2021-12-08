8d633dc0b831efdb79db3034ed12858cf42bb484
AndrieievaLena <elenakovalevamod@gmail.com>
2021-12-08 22:22:04 +0200

    added feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const storageDate = "feedback-form-state";
+
+const localStorageDate = {
+    email: '',
+    message: '',
+};
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+
+}
+refs.form.addEventListener('submit', onFormSubmit);
+saveData()
+
+refs.email.addEventListener('input', throttle((e) => {
+    localStorageDate.email = e.target.value;
+    localStorage.setItem(storageDate, JSON.stringify(localStorageDate));
+}, 500));
+
+
+refs.message.addEventListener('input', throttle((e) => {
+    localStorageDate.message = e.target.value;
+    localStorage.setItem(storageDate, JSON.stringify(localStorageDate));
+}, 500));
+
+ function onFormSubmit (e){
+    e.preventDefault();
+
+    if (localStorage.getItem(storageDate)) {
+        console.log(localStorage.getItem(storageDate))
+    }
+    e.currentTarget.reset();
+    localStorage.removeItem(storageDate)
+};
+
+function saveData () {
+    let savedData = JSON.parse(localStorage.getItem(storageDate))
+    if (savedData) {
+        localStorageDate.email = savedData.email;
+        localStorageDate.message = savedData.message;
+        refs.email.value = localStorageDate.email;
+        refs.message.value = localStorageDate.message;
+    }
+}
